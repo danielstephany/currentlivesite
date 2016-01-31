4fe06d7abf8828b239de83a32e7bbfd6e8f2ae0c
Daniel <danielstephany85@gmail.com>
2016-01-30 22:01:14 -0600

    backup commit

---
+++ mud-creek-min/js/nav.js
@@ -0,0 +1,1 @@
+var main=function(){$("#open-toggle").click(function(){$("#side-nav").animate({right:"0px"},"slow");$("#open-toggle").hide()});$("#close-toggle").click(function(){$("#side-nav").animate({right:"-200px"},"slow");$("#open-toggle").show()});};$("document").ready(main());
